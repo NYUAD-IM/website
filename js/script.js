f3820f1ec6ed75690d294e31ffc69c7fd90556ef
pierre <pierre.depaz@gmail.com>
2018-02-10 19:42:35 +0400

    more relative paths

--- js/script.js
+++ js/script.js
@@ -11,13 +11,13 @@ $(document).ready(function(){
 	console.log("Page ready");
 
     //Inject resusable HTML 
-    $('#header-container').load('/html/navigation.html');
+    $('#header-container').load('./html/navigation.html');
 
 
 	// Load overlays as HTML snippets (this way allows us to reuse the overlays across the site)
-	$('#overlay-description').load('html/overlay-description.html');
-	$('#overlay-examples').load('html/overlay-examples.html');
-	$('#overlay-participation').load('html/overlay-participation.html');
+	$('#overlay-description').load('./html/overlay-description.html');
+	$('#overlay-examples').load('./html/overlay-examples.html');
+	$('#overlay-participation').load('./html/overlay-participation.html');
 
 	//add event listener to background div (beneath overlays) to allow an outside click to close overlay
 	document.getElementById("overlay-background").addEventListener("click", overlayClose);
