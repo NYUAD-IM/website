1f80a55bc6e0ef3781fc07568420419ab3fde2d4
pierre <pierre.depaz@gmail.com>
2018-02-12 16:31:13 +0400

    added nicer navigation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
 	console.log("Welcome to IM!");
 
-    //Inject resusable HTML
-    $('#header-container').load('/html/navigation.html');
-    $('#footer-container').load('/html/footer.html');
+    	//Inject resusable HTML
+    	$('#header-container').load('/html/navigation.html');
+    	$('#footer-container').load('/html/footer.html');
+    
 });
+
+let showNavigation = () => {
+	if($('#nav-dropdown-list').css('height') == '0px' || $('#nav-dropdown-list').css('height') == '3px'){//show it
+
+    		$('#nav-dropdown-list').css('height', '40px');
+
+	}else{ //hide it
+		$('#nav-dropdown-list').css('height', '0px');
+	}
+}
