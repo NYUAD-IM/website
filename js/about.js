9d524b98404e04fe40d2fe57fd2cb364df06d183
pierre <pierre.depaz@gmail.com>
2018-02-13 00:47:39 +0400

    first pass about page

---
+++ js/about.js
@@ -0,0 +1,28 @@
+$(document).ready(() => {
+	attachListener()
+})
+
+let attachListener = () => {
+
+	document.body.onmousemove = (e) => {
+		let x = (e.clientX / window.innerWidth)*127
+		let y = (e.clientY / window.innerHeight)*127
+		let sum = 255 - (x+y)
+		let col = 'rgb('+sum+','+sum+','+sum+')'
+
+		$('body').css('background-color', col)
+	}
+}
+
+let expand = (el) => {
+	if(el.style.height == '100px')
+		el.style.height = '500px'
+	else
+		el.style.height = '100px'
+}
+
+let colorize = (color) => {
+	document.body.onmousemove = null
+	document.body.style.backgroundColor = color
+	console.log(color);
+}
