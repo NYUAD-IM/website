3010c1fb67a338f78322ee6ef7709a3ab990d4c1
pierre <pierre.depaz@gmail.com>
2018-02-15 00:51:19 +0400

    basic academic fetch

---
+++ js/opportunities.js
@@ -0,0 +1,13 @@
+let container
+
+$(document).ready(() => {
+	fetch()
+	container = document.getElementById('academics-container')
+})
+
+let populate = (d) => {
+	const CONTENT = d
+
+	console.log(CONTENT)
+
+}
