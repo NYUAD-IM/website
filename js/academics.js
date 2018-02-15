a86fc4665fb7ac16d6cd0a749c1df640d6afd8d2
pierre <pierre.depaz@gmail.com>
2018-02-15 20:08:00 +0400

    added gallery to dropdown

--- js/academics.js
+++ js/academics.js
@@ -10,4 +10,22 @@ let populate = (d) => {
 
 	console.log(CONTENT)
 
+	for(let course of CONTENT)
+		addElement(course)
+
+}
+
+let addElement = (course) => {
+	//title
+	//program
+	//track
+	//instructors
+	//terms
+	//link
+	//description short
+	//description long
+	//cross listed
+	//tags
+	
+	//concat the track+semester to add the class(foundation, elective, cross-listed) + (fall, spring, etc)
 }
