34e44207156357829037b9c280fea4f9da776865
pierre <pierre.depaz@gmail.com>
2018-03-20 21:54:12 +0400

    updated main sketch to lower count and added width on people description

--- js/sketches/sketch.js
+++ js/sketches/sketch.js
@@ -60,9 +60,9 @@ function customSetup(){
 
 function draw(){
 	background(255)
-	strokeWeight(55)
-	for(var x = 0; x < width*1.2; x += 60){
-		for(var y = 0; y < height*1.2; y += 60){
+	strokeWeight(75)
+	for(var x = 0; x < width*1.2; x += 90){
+		for(var y = 0; y < height*1.2; y += 90){
 			stroke(map(x, 0, width, 0, 255), map(y, 0, height, 0, 255), 127, 200)
 			line(x, y, lerp(x, mouseX, 0.05), lerp(y, mouseY, 0.05))
 		}
