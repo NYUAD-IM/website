5a4ec493401bf90c489d30a3904db032b82eed2e
NYUAD <nyuad@C3-029-MBP-14.local>
2018-03-20 15:18:41 +0400

    added overlay to academics, still needs more tweeking to make transition smoother

--- js/fetch.js
+++ js/fetch.js
@@ -3,8 +3,10 @@ let fetch = () => {
     url: TARGET_URL,
     dataType: 'json'
   }).done((data) => {
+  	$(".load-container").delay(500).fadeOut();
     populate(data)
   }).fail(() => {
     console.log('fetching data from',TARGET_URL,'failed...');
   })
 }
+
