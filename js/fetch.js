a6178ee96db3655083f670a726418d1036d17b45
pierre <pierre.depaz@gmail.com>
2018-03-27 23:34:00 +0400

    added background and put on all pages

--- js/fetch.js
+++ js/fetch.js
@@ -3,13 +3,21 @@ let fetch = () => {
     url: TARGET_URL,
     dataType: 'json'
   }).done((data) => {
-    loader()
+    fadeOutLoader()
     populate(data)
   }).fail(() => {
     console.log('fetching data from',TARGET_URL,'failed...');
   })
 }
 
-function loader() {
-  document.getElementById("loader").style.display = "none";
+function fadeOutLoader() {
+  //-- first fade out
+  document.getElementById("load-bg").style.opacity = 0;
+  document.getElementById("load-anim").style.opacity = 0;
+
+  //-- 0.4s later, make it disappear
+  setTimeout(() => {
+    document.getElementById("load-bg").style.display = 'none';
+    document.getElementById("load-anim").style.display = 'none';
+  }, 400)
 }
