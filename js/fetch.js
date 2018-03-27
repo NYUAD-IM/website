649ee4593ce90cf951265940a0ef7b1a94441a99
arantzardzm <arantzardzm@hotmail.com>
2018-03-27 13:07:38 +0400

    loader modifications in academics page

--- js/fetch.js
+++ js/fetch.js
@@ -3,10 +3,13 @@ let fetch = () => {
     url: TARGET_URL,
     dataType: 'json'
   }).done((data) => {
-  	$(".load-container").delay(500).fadeOut();
+    loader()
     populate(data)
   }).fail(() => {
     console.log('fetching data from',TARGET_URL,'failed...');
   })
 }
 
+function loader() {
+  document.getElementById("loader").style.display = "none";
+}
