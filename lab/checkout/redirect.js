dea510c9394526f3d5c4220b1f2e8da0e1b3dcfe
Jack B. Du <jackbdu@nyu.edu>
2019-09-12 13:11:45 +0400

    checkout redirects to artsbooking

--- lab/checkout/redirect.js
+++ lab/checkout/redirect.js
@@ -1,4 +1,5 @@
-window.location = "https://goo.gl/forms/LPMbIr5MPT2H76af1";
+//window.location = "https://goo.gl/forms/LPMbIr5MPT2H76af1";
 // window.location.href = "http://nyuad.im/24x";
 // window.location.assign("http://nyuad.im/24x");
 // window.location.replace("http://nyuad.im/24x");
+window.location = "https://nyuad-artsbooking.nyu.edu/";
